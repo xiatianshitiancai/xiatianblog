a3b203dd8b3eabe483db0bbfd58b33847cce6b46
xiazhiyong <xzy406728963@163.com>
2021-01-12 17:21:40 +0800

    fix

--- utils/baiduPush.js
+++ utils/baiduPush.js
@@ -9,6 +9,7 @@ const readFileList = require('./modules/readFileList');
 const urlsRoot = path.join(__dirname, '..', 'urls.txt'); // 百度链接推送文件
 const DOMAIN = process.argv.splice(2)[0]; // 获取命令行传入的参数
 
+
 if (!DOMAIN) {
   console.log(chalk.red('请在运行此文件时指定一个你要进行百度推送的域名参数，例：node utils/baiduPush.js https://xiatianshitiancai.github.io/xiazhiyong'))
   return
